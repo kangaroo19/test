43570882f1d06c55835ef330553ad77cf99c0642
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-22 00:33:39 +0900

    123

--- js/main.js
+++ js/main.js
@@ -47,14 +47,14 @@ function transformNext(event){
     classList.setAttribute('data-position',activeLi);
 }
 /////////////////////////////////////////////////////////////////////
-function transformPrev(event){
+function transformPrev(event){   ///<-----방향(왼쪽)
     const slidePrev=event.target; //해당 이벤트가 발생된 요소 가져올수있음
     const slideNext=slidePrev.nextElementSibling;
     //console.log(sildePrev);
     //console.log(sildeNext);
 
     const classList=slidePrev.parentElement.parentElement.nextElementSibling;
-    let activeLi=classList.getAttribute('data-position');
+    let activeLi=classList.getAttribute('data-position'); //디폴트 값은 0
     const liList=classList.getElementsByTagName('li');
     //console.log(liList.length*260*Number(activeLi))
     /*
@@ -65,7 +65,10 @@ function transformPrev(event){
     */
     if(classList.clientWidth<(liList.length * 260 + Number(activeLi))){ //넘칠때
         //위치를 왼쪽으로 260 이동(-260px);
+        
         activeLi=Number(activeLi)-260;
+        console.log(activeLi);
+
        // console.log(activeLi);
         /*위치를 왼쪽으로 260 이동(-260px)
         해당 위치는 변경된 activeLi값이 적용된 liList.length*260+Number(activeLi) 값임
@@ -74,7 +77,7 @@ function transformPrev(event){
         */
 
         if(classList.clientWidth>(liList.length * 260 + Number(activeLi))){
-            slidePrev.style.color='#cfd8dc'; //회색(비활성)
+            slidePrev.style.color='#cfd8dc'; //왼쪽버튼 비활성(회색)
             slidePrev.classList.remove('slide-prev-hover');
             slidePrev.removeEventListener('click',transformPrev);
         }
@@ -87,18 +90,18 @@ function transformPrev(event){
     classList.setAttribute('data-position',activeLi);
 }
 
-const slidePrevList=document.getElementsByClassName("slide-prev");
+const slidePrevList=document.getElementsByClassName("slide-prev");//slidePrevList에는 slide-prev가 3개 이므로 배열형식으로 3개 저장
 
 for(let i=0; i<slidePrevList.length; i++){
     let classList=slidePrevList[i].parentElement.parentElement.nextElementSibling;
     let liList=classList.getElementsByTagName('li');
     console.log('classList.clientWidth='+classList.clientWidth);
     console.log('liList의 길이 * 260='+liList.length*260);
-    if(classList.clientWidth<liList.length * 260){  //li요소(class-card)가 넘칠때
+    if(classList.clientWidth<liList.length * 260){  //li요소(class-card)가 넘칠때 ,260은 하나의 클래스카드의 width
         slidePrevList[i].classList.add("slide-prev-hover");
         slidePrevList[i].addEventListener('click',transformPrev);
     }
-    else{
+    else{//넘치지 않으면 arrowcontainer의 자식요소 삭제(자식요소의 형제요소부터 삭제할 것)
         const arrowContainer=slidePrevList[i].parentElement;
         arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
         arrowContainer.removeChild(slidePrevList[i]);
--- test.js
+++ test.js
@@ -9,3 +9,26 @@ box1.setAttribute('data-position',2);
 console.log(box1.getAttribute('data-position'));
 const a=box1.getAttribute('data-position');
 console.log(a);
+
+
+const container=document.getElementsByClassName('container');
+for(let i=0;i<container.length;i++){
+    console.log(i);
+}
+
+const move=document.getElementById('move');
+const controller=document.getElementById('controller');
+move.style.border='1px solid red';
+function moveCircle(){
+    move.style.transition='transform 0.5s';
+    move.style.transform='translateX('+b*50+'px)';
+
+}
+let b=1;
+function countClick(){
+    //b=1;
+    console.log(b);
+    b++;
+    controller.addEventListener('click',moveCircle);
+}
+controller.addEventListener('click',countClick);
