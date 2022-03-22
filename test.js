e03ff7bb3521a1d30a21a2715ddee7ce0cb7c3be
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-22 23:22:54 +0900

    123

--- test.js
+++ test.js
@@ -1,34 +1,9 @@
+const box3=document.getElementById("box3");
+const userWindow=document.getElementById("window");
 
 
 
-const box1=document.querySelector("#box2");
+box3.addEventListener("dblclick",()=>{
+    console.log("dblclicked");
+});
 
-box1.setAttribute('data-position',2);
-
-
-console.log(box1.getAttribute('data-position'));
-const a=box1.getAttribute('data-position');
-console.log(a);
-
-
-const container=document.getElementsByClassName('container');
-for(let i=0;i<container.length;i++){
-    console.log(i);
-}
-
-const move=document.getElementById('move');
-const controller=document.getElementById('controller');
-move.style.border='1px solid red';
-function moveCircle(){
-    move.style.transition='transform 0.5s';
-    move.style.transform='translateX('+b*50+'px)';
-
-}
-let b=1;
-function countClick(){
-    //b=1;
-    console.log(b);
-    b++;
-    controller.addEventListener('click',moveCircle);
-}
-controller.addEventListener('click',countClick);
