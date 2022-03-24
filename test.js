92ba62ebaa5e651ec8148ca8fcbb03df9f4f4834
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-24 23:26:31 +0900

    123

--- test.js
+++ test.js
@@ -1,34 +1,11 @@
-const ball=document.getElementById("ball");
+const sectionImg=document.querySelector("#window-section-img");
+const sectionText=document.querySelector("#window-section-section");
 
-ball.onmousedown=function(event){
-    let shiftX=event.clientX-ball.getBoundingClientRect().left;
-    let shiftY=event.clientY-ball.getBoundingClientRect().top;
-    console.log(shiftX,event.clientX);
-    ball.style.position='absolute';
-    ball.style.zIndex=1000;
-    document.body.append(ball);
-    
-    function moveAt(pageX,pageY){
-        ball.style.left=pageX-shiftX+'px';
-        ball.style.top=pageY-shiftY+'px';
-        
-    }
-
-    moveAt(event.pageX,event.pageY);
-
-    function onMouseMove(event){
-        moveAt(event.pageX,event.pageY);
-    }
-    document.addEventListener('mousemove',onMouseMove);
-
-    ball.onmouseup=function(){
-        document.removeEventListener('mousemove',onMouseMove);
-       // ball.onmouseup=null;
-    };
-};
-
-ball.ondragstart=function(){
-    return false;
-}
+const imgAndText=[
+    {
+        image:"../img/momentum.PNG",
+        text:"this is momentum"
+    },
+]
 
 
