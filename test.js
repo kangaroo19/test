e37f508ee76faabf31e868cf309f562a6df8cb06
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-24 21:06:13 +0900

    123

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 const ball=document.getElementById("ball");
 
 ball.onmousedown=function(event){
+    let shiftX=event.clientX-ball.getBoundingClientRect().left;
+    let shiftY=event.clientY-ball.getBoundingClientRect().top;
+    console.log(shiftX,event.clientX);
     ball.style.position='absolute';
     ball.style.zIndex=1000;
     document.body.append(ball);
     
     function moveAt(pageX,pageY){
-        ball.style.left=pageX-ball.offsetWidth/2+'px';
+        ball.style.left=pageX-shiftX+'px';
+        ball.style.top=pageY-shiftY+'px';
+        
     }
 
     moveAt(event.pageX,event.pageY);
@@ -14,7 +19,16 @@ ball.onmousedown=function(event){
     function onMouseMove(event){
         moveAt(event.pageX,event.pageY);
     }
-}
+    document.addEventListener('mousemove',onMouseMove);
 
+    ball.onmouseup=function(){
+        document.removeEventListener('mousemove',onMouseMove);
+       // ball.onmouseup=null;
+    };
+};
+
+ball.ondragstart=function(){
+    return false;
+}
 
 
