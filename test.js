b48b30a4e64ec0e08208414814798d8050ef14f4
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-03-25 23:17:34 +0900

    123

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
-const sectionImg=document.querySelector("#window-section-img");
-const sectionText=document.querySelector("#window-section-section");
-
-const imgAndText=[
-    {
-        image:"../img/momentum.PNG",
-        text:"this is momentum"
-    },
-]
+const button=document.querySelector("#button");
 
 
+button.disabled=true;
