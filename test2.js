cfa18f92b1567b24122703c437dc42d36c04afab
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2022-06-23 04:47:37 +0900

    123

---
+++ test2.js
@@ -0,0 +1,5 @@
+//실제 문제 풀때는 example.txt -> /dev/stdin
+let input = require('fs').readFileSync('example.txt').toString().split('\n');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
